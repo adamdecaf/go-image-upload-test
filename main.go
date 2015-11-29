d65e62bcf25160f5c0a17fb4b0272ea322e41a81
Adam Shannon <adam@ashannon.us>
2015-11-29 15:53:24 -0600

    main: serve static files off /i/ path

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	// set routes
 	http.Handle("/", http.FileServer(http.Dir("./html/")))
 	http.HandleFunc("/ping", routes.Ping)
+	http.Handle("/i/", http.StripPrefix("/i/", http.FileServer(http.Dir("./tmp"))))
 	http.HandleFunc("/upload", routes.Upload)
 
 	fmt.Printf("Starting http server on :8080\n")
